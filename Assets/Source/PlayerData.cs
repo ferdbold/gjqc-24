e3276af38daf586d8af0d9cfbfedbcdfb57c7647
jean-nicolas <jean-nicolas_ranger@anciens-mss.ca>
2024-10-05 13:04:40 -0400

    Healthbarv2

--- Assets/Source/PlayerData.cs
+++ Assets/Source/PlayerData.cs
@@ -1,5 +1,10 @@
 using Unity.Properties;
 using UnityEngine;
+using UnityEngine.UIElements;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 [CreateAssetMenu(fileName = "Player", menuName = "Jam/Player")]
 public class PlayerData : ScriptableObject
@@ -18,4 +23,16 @@ public class PlayerData : ScriptableObject
     }
 
     [CreateProperty] public bool Stunned => StunProgress >= 0;
+
+#if UNITY_EDITOR
+    [InitializeOnLoadMethod]
+#else
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+#endif
+    public static void RegisterConverters()
+    {
+        var floatToStyleLength = new ConverterGroup("Float to StyleLength Percent");
+        floatToStyleLength.AddConverter((ref float val) => new StyleLength(new Length(val, LengthUnit.Percent)));
+        ConverterGroups.RegisterConverterGroup(floatToStyleLength);
+    }
 }
